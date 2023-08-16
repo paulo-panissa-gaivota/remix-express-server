9dae87b364136b858b2b7e4275597c2ff0e2aa9e
Paulo Panissa <paulo.panissa@gaivota.ai>
2023-08-16 11:57:47 -0300

    test

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import { HarvestThemeProvider } from "@gaivota/harvest";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        {typeof document === "undefined" ? "__STYLES__" : null}
+      </head>
+      <body>
+        <HarvestThemeProvider theme="seedz">
+          <Outlet />
+        </HarvestThemeProvider>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
